9f4a9c54c66f0dd484db2cd1af0f9d05af24bdbb
amnwigwe <72475284+amnwigwe@users.noreply.github.com>
2022-03-20 21:57:47 -0400

    create main.cpp

---
+++ src/templatedb/main.cpp
@@ -0,0 +1,17 @@
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <sstream>
+#include <unordered_map>
+#include <vector>
+
+#include "operation.hpp"
+
+using namespace std;
+
+int main(){
+    templatedb::Operation test;
+    cout << test.type;
+    return 0;
+}
